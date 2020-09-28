486981f1883c83670e8bd9be6315380834f7a47b
manikandabose <38509111+manikandabose@users.noreply.github.com>
2020-09-28 20:52:51 +0530

    removed logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import ProfileImage from './images/profile.png';
 
@@ -8,7 +7,7 @@ function App() {
     <div className="App">
       <h1>My Application</h1>
       <h2>Author: Manikandabose</h2>
-      <img src={ProfileImage} alt="profile-image" />
+      <img src={ProfileImage} alt="profile" />
     </div>
   );
 }
